8329eb20b59564dd377274609a582861dd5548f3
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-10-21 14:10:03 -0700

    patrolling and chase navmesh ai

---
+++ MegaKill-ULTRA v4/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyPatrol : MonoBehaviour
+{
+    GameObject player;
+
+    NavMeshAgent agent;
+
+    [SerializeField] LayerMask groundLayer, playerLayer;
+
+    //patrol
+    Vector3 destPoint;
+    bool walkpointSet;
+    [SerializeField] float range;
+
+    //state change
+    [SerializeField] float sightRange;
+    bool playerInSight;
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        if (!playerInSight) Patrol();
+
+        if (playerInSight) Chase();
+      
+    }
+        
+
+    void Patrol()
+    {
+        if (!walkpointSet) DestSearch();
+        if (walkpointSet) agent.SetDestination(destPoint);
+        if (Vector3.Distance(transform.position, destPoint) < 10) walkpointSet = false;
+    }
+    void DestSearch()
+    {
+        float z = Random.Range(-range, range);
+        float x = Random.Range(-range, range);
+
+        destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+        if(Physics.Raycast(destPoint, Vector3.down, groundLayer))
+        {
+            walkpointSet = true;
+        }
+    }
+
+    void Chase()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
