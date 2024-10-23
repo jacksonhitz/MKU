ca55765719bb24a85c81ea1bd08fd84d26ec728b
jacksonhitz <jacksonhitz@gmail.com>
2024-10-23 15:06:43 -0700

    music // fuck A*

---
+++ MegaKill-ULTRA v4/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource music;
+    AudioSource sfx;
+
+    public AudioClip title;
+    public AudioClip witch;
+    public AudioClip hott;
+    public AudioClip threes;
+    
+
+}
