8b702c7e19f85a648ac9dae7f866b63697653cc3
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-10-30 23:35:29 -0700

    updated game

--- MegaKill-ULTRA v4/Assets/Scripts/EnemyPatrol.cs
+++ MegaKill-ULTRA v4/Assets/Scripts/EnemyPatrol.cs
@@ -1,31 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class EnemyPatrol : MonoBehaviour
 {
     GameObject player;
-    NavMeshAgent agent;
-    GameManager gameManager;
 
+    NavMeshAgent agent;
+   
+    GameManager gameManager;
+    
     [SerializeField] LayerMask groundLayer, playerLayer;
 
-    // Patrol
-    [SerializeField] Transform[] waypoints; // Array to store waypoints
-    int currentWaypointIndex = 0;
+    //patrol
+    Vector3 destPoint;
+    bool walkpointSet;
+    [SerializeField] float range;
     [SerializeField] float patrolSpeed = 3.5f;
     [SerializeField] float chaseSpeed = 6.0f;
-
-    // State change
+    //state change
     [SerializeField] float sightRange;
     [SerializeField] float attackRange;
     bool playerInAttackRange;
+    //[SerializeField] bool playerInSight;
     [SerializeField] bool isHostile;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {  
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
         gameManager = FindObjectOfType<GameManager>();
@@ -35,41 +39,42 @@ public class EnemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
-
         if (!isHostile) Patrol();
         if (isHostile && gameManager.score == 0) Patrol();
         if (isHostile && gameManager.score > 0) Chase();
         if (isHostile && playerInAttackRange && gameManager.score > 0) Attack();
     }
+        
 
     void Patrol()
     {
         agent.speed = patrolSpeed;
+        if (!walkpointSet) DestSearch();
+        if (walkpointSet) agent.SetDestination(destPoint);
+        if (Vector3.Distance(transform.position, destPoint) < 10) walkpointSet = false;
+    }
+    void DestSearch()
+    {
+        float z = Random.Range(-range, range);
+        float x = Random.Range(-range, range);
 
-        if (!agent.hasPath || agent.remainingDistance < 1f)
+        destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+        if(Physics.Raycast(destPoint, Vector3.down, groundLayer))
         {
-            SetNextWaypoint();
+            walkpointSet = true;
         }
     }
 
-    void SetNextWaypoint()
-    {
-        if (waypoints.Length == 0) return;
-
-        // Select a random waypoint
-        currentWaypointIndex = Random.Range(0, waypoints.Length);
-        agent.SetDestination(waypoints[currentWaypointIndex].position);
-    }
-
-    void Attack()
-    {
-        agent.SetDestination(transform.position);
-    }
-
+     void Attack()
+     {
+         agent.SetDestination(transform.position);
+     }
     void Chase()
     {
-        agent.speed = chaseSpeed;
+        agent.speed = chaseSpeed; 
         agent.SetDestination(player.transform.position);
     }
 }
