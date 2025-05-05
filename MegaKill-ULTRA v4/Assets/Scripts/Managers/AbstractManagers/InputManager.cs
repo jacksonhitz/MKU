84f45b20a666db026b231fb03bacdd24ccdaf376
jacksonhitz <jacksonhitz@gmail.com>
2025-05-04 19:51:30 -0700

    temp fix

--- MegaKill-ULTRA v4/Assets/Scripts/Managers/AbstractManagers/InputManager.cs
+++ MegaKill-ULTRA v4/Assets/Scripts/Managers/AbstractManagers/InputManager.cs
@@ -1,43 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class InputManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        UpdateInputs();
+        
     }
-
-    protected void UpdateInputs()
-    {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if (StateManager.State == StateManager.GameState.TITLE)
-            {
-                StateManager.LoadState(StateManager.GameState.INTRO);
-            }
-            else if (StateManager.State == StateManager.GameState.INTRO)
-            {
-                StateManager.LoadState(StateManager.GameState.TUTORIAL);
-            }
-            else if (StateManager.State == StateManager.GameState.TUTORIAL)
-            {
-                StateManager.LoadState(StateManager.GameState.TANGO);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (StateManager.State != StateManager.GameState.PAUSED)
-            {
-                StateManager.LoadState(StateManager.GameState.PAUSED);
-            }
-            else
-            {
-                // FIX LATER
-                
-            }
-        }
-    }
-
-    void 
 }
