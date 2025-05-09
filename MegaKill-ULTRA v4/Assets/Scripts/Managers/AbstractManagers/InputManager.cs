1531a2e7278abcfb9364a71808537cff45d1a977
Niko Nissen <nikonissen@yahoo.com>
2025-05-08 19:50:31 -0700

    Pre Power Outage Patch - Test 1
    Please dont break anything..
    Swapped the "Assets", and "ProjectSettings" folders with the versions from the backup.

--- MegaKill-ULTRA v4/Assets/Scripts/Managers/AbstractManagers/InputManager.cs
+++ MegaKill-ULTRA v4/Assets/Scripts/Managers/AbstractManagers/InputManager.cs
@@ -1,39 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class InputManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        UpdateBase();
-        UpdateItems();
-        UpdatePlayer();
+        
     }
-
-    void UpdateBase()
-    {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if (StateManager.State == StateManager.GameState.TITLE)
-                StateManager.LoadState(StateManager.GameState.INTRO);
-            else if (StateManager.State == StateManager.GameState.INTRO)
-                StateManager.LoadState(StateManager.GameState.TUTORIAL);
-            else if (StateManager.State == StateManager.GameState.TUTORIAL)
-                StateManager.LoadState(StateManager.GameState.TANGO);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (StateManager.State != StateManager.GameState.PAUSED)
-                StateManager.LoadState(StateManager.GameState.PAUSED);
-            else
-            {
-                // Handle unpausing or returning to previous state
-            }
-        }
-    }
-
-    protected abstract void UpdateItems();
-    protected abstract void UpdatePlayer();
 }
-
-
