a7b43c3aa360670adf6cc5644e7db90634e06d9d
jacksonhitz <jacksonhitz@gmail.com>
2025-06-02 17:05:02 -0700

    organizing

---
+++ MegaKill-ULTRA v4/Assets/Scripts/Data/SettingsData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Settings")]
+public class SettingsData : ScriptableObject
+{
+    
+
+    public AudioClip[] clips;
+    public float volume;
+}
+
+
